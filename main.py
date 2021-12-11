aba7c648c618f8d1b9615221a1f18730be376136
Sho Nakatani <lay.sakura@gmail.com>
2021-12-11 21:22:18 +0900

    chore: larger hight

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ app.layout = html.Div([
     cyto.Cytoscape(
         id='cytoscape-two-nodes',
         layout={'name': 'preset'},
-        style={'width': '100%', 'height': '400px'},
+        style={'width': '100%', 'height': '800px'},
         elements=[
-            {'data': {'id': 'one', 'label': 'Node 1'}, 'position': {'x': 75, 'y': 75}},
-            {'data': {'id': 'two', 'label': 'Node 2'}, 'position': {'x': 200, 'y': 200}},
+            {'data': {'id': 'one', 'label': 'Node 1'},
+                'position': {'x': 75, 'y': 75}},
+            {'data': {'id': 'two', 'label': 'Node 2'},
+                'position': {'x': 200, 'y': 200}},
             {'data': {'source': 'one', 'target': 'two'}}
         ]
     )

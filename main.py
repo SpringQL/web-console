5c69b49e64e35cdb13779ca924b3de6a7e0e2bd3
Sho Nakatani <lay.sakura@gmail.com>
2021-12-11 21:30:55 +0900

    feat: BFS layout

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ app = dash.Dash(__name__)
 app.layout = html.Div([
     cyto.Cytoscape(
         id='cytoscape-two-nodes',
-        layout={'name': 'preset'},
+        layout={
+            'name': 'breadthfirst',
+            'roots': '#one'
+        },
         style={'width': '100%', 'height': '800px'},
         elements=[
-            {'data': {'id': 'one', 'label': 'Node 1'},
-                'position': {'x': 75, 'y': 75}},
-            {'data': {'id': 'two', 'label': 'Node 2'},
-                'position': {'x': 200, 'y': 200}},
+            {'data': {'id': 'one', 'label': 'Node 1'}},
+            {'data': {'id': 'two', 'label': 'Node 2'}},
             {'data': {'source': 'one', 'target': 'two'}}
         ]
     )
